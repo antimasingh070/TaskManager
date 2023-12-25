a507bf90c0cfe14535eb979332e3123e6e6490b1
antimasingh070 <antimasingh720@gmail.com>
2023-12-25 15:43:03 +0530

    fdjskdf

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       resources :users, only: [:create, :show, :destroy, :update, :index] do
         collection do 
           get :login
+          post :reset_password
+          post :sign_up
         end
       end
 
